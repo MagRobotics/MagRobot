0945b1e3cc90c34c8fc203899d0f4b93bdd8ca71
MagRobotics <161809470+MagRobotics@users.noreply.github.com>
2025-11-12 19:07:13 +0800

    Update main.py

--- demo_c/main.py
+++ demo_c/main.py
@@ -5,6 +5,8 @@ import simulator_setting
 import create_scene
 import post_processing
 import animate_controller
+import animate_controller_21
+import animate_controller_22
 import path_controller
 import position_estimate
 import field_show
@@ -19,12 +21,12 @@ def createScene(root_node):
     # f = open("GUI_interface/info_location.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_Helmholtz_Maxwell.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_wire_elc_hard_brains_nc_peitu1.txt", "r", encoding="UTF-8")
-    # f = open("GUI_interface/info_capsule_elc_soft_stomach.txt", "r", encoding="UTF-8")
+    # f = open("GUI_interface/info_capsule_elc_soft_stomach_nc_peitu3.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_wire_elc_soft_brains_nc.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_wire_elc_hard_bronchus.txt", "r", encoding="UTF-8")
-    # f = open("GUI_interface/info_wire_rob_hard_bronchus2_nc.txt", "r", encoding="UTF-8")
+    # f = open("GUI_interface/info_wire_rob_hard_bronchus_nc.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_wire_elc_hard_arc.txt", "r", encoding="UTF-8")
-    # f = open("GUI_interface/info_wire_elc_soft_arc_nc.txt", "r", encoding="UTF-8")
+    # f = open("GUI_interface/info_wire_elc_soft_arc3_nc.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_capsule_rob.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_paper.txt", "r", encoding="UTF-8")
     # f = open("GUI_interface/info_wire_elc_soft_arc_nc_key.txt", "r", encoding="UTF-8")
@@ -32,6 +34,10 @@ def createScene(root_node):
     # f = open("GUI_interface/info_wire_rob_hard_bronchus_shiyan.txt", "r", encoding="UTF-8")
 
     # f = open("GUI_interface/video/info_wire_rob_hard_bronchus_nc.txt", "r", encoding="UTF-8")
+
+    # f = open("GUI_interface/info_capsule_rob.txt", "r", encoding="UTF-8")
+    # f = open("GUI_interface/gai_demo/info_capsule_rob.txt", "r", encoding="UTF-8")
+    # f = open("GUI_interface/gai_demo/info_wire_elc_soft_arc3_nc.txt", "r", encoding="UTF-8")
 
     json_str = f.read()
     initial_info = json.loads(json_str)
@@ -56,21 +62,31 @@ def createScene(root_node):
 
     if path and pose_estimate:
         # 循环
-        mag_controller = animate_controller.MagController(root_node, info=initial_info, simulator=simulator, scene=scene, path=path, pose_estimate=pose_estimate)
+        if len(initial_info[1]) == 2 and len(initial_info[2]) == 2:
+            mag_controller = animate_controller_22.MagController(root_node, info=initial_info, simulator=simulator,
+                                                              scene=scene, path=path,
+                                                              pose_estimate=pose_estimate)
+        elif len(initial_info[1]) == 2 and len(initial_info[2]) == 1:
+            mag_controller = animate_controller_21.MagController(root_node, info=initial_info, simulator=simulator,
+                                                              scene=scene, path=path,
+                                                              pose_estimate=pose_estimate)
+        else:
+            mag_controller = animate_controller.MagController(root_node, info=initial_info, simulator=simulator, scene=scene, path=path,
+                                                          pose_estimate=pose_estimate)
         root_node.addObject(mag_controller)
 
         # 后处理
-        post = post_processing.Post_processing(scene, path, pose_estimate, mag_controller=mag_controller, environment=simulator)
-        root_node.addObject(post)
-
-        # 监视
+        # post = post_processing.Post_processing(scene, path, pose_estimate, mag_controller=mag_controller, environment=simulator)
+        # root_node.addObject(post)
+        # #
+        # # 监视
         # monitor = Monitor.Monitor(root_node, scene, post)
         # root_node.addObject(monitor)
 
     # 磁场显示
     # if initial_info[5][0]["field_show"] != 0:
     #     for i in range(len(initial_info[5][0]["field_show"])):
-    #         field = field_show.magnetic_field_show(root_node, initial_info[5][0]["field_show"][i], scene, initial_info)
+    #         field = field_show.magnetic_field_show(root_node, initial_info[5][0]["field_show"][i], scene)
     #         root_node.addObject(field)
 
     # 操作记录
